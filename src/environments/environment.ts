4a6271aeb668843a2b3855b42b574f5e1e2c31e3
Subham Dhiyotech <shubham@dhiyotech.in>
2024-01-13 13:14:09 +0530

    First Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLkvpuD0zPJXw4ZlN8ZgNekMM4Lph67mM",
+    authDomain: "education-lms-1701250931421.firebaseapp.com",
+    projectId: "education-lms-1701250931421",
+    storageBucket: "education-lms-1701250931421.appspot.com",
+    messagingSenderId: "792748978749",
+    appId: "1:792748978749:web:7511b6daa467d98ba83054",
+    measurementId: "G-PVM4BH9ERJ"
+  }
 };
 
 /*
