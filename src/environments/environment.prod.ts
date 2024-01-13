aab9a424b738591b1f99820b67d864e8941b92b0
Subham Dhiyotech <shubham@dhiyotech.in>
2024-01-13 16:04:54 +0530

    Added details in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://learn.fukeyeducation.com/',
+  BASE_API_URL: 'https://learn.fukeyeducation.com/api/',
+  firebaseConfig: {
+    apiKey: "AIzaSyDLkvpuD0zPJXw4ZlN8ZgNekMM4Lph67mM",
+    authDomain: "education-lms-1701250931421.firebaseapp.com",
+    projectId: "education-lms-1701250931421",
+    storageBucket: "education-lms-1701250931421.appspot.com",
+    messagingSenderId: "792748978749",
+    appId: "1:792748978749:web:7511b6daa467d98ba83054",
+    measurementId: "G-PVM4BH9ERJ"
+  },
+  countryJson: [
+		{ name: 'India', dial_code: '+91', code: 'IN' },
+	]
 };
